79671c834c8a553d5032c8a769ea95889bd8d34f
Jay Cox <j.cox@awl.us>
2019-04-24 10:49:54 -0400

    Version 1.0 Master Commit

--- IPMonitoring/Pages/MonitorIPViewModel.cs
+++ IPMonitoring/Pages/MonitorIPViewModel.cs
@@ -11,26 +11,38 @@ using System.Windows.Controls;
 using IPMonitoring.Models;
 using Stylet;
 using IPMonitoring.Pages;
+using System.Windows;
+using System.Windows.Input;
+using System.Text.RegularExpressions;
 
 namespace IPMonitoring.Pages
 {
     public class MonitorIPViewModel : Screen
     {
+        #region Fields
         private List<IPAddress> IpAddressList;
         private Task<List<PingReply>> Responses;
         private string inputProjectNumber;
         private Thread PingThread;
         private SynchronizationContext uiContext;
+        private Visibility refreshRateInputVisibilty;
         private IEventAggregator eventAggregator;
+        private string refreshRateInput;
+        private int internalRefreshRate; 
+        #endregion
 
-        public MonitorIPViewModel(string filePath)
+        #region Constructor
+        public MonitorIPViewModel(string filePath, IEventAggregator eventAggregator)
         {
-            this.eventAggregator = new EventAggregator();
+            this.eventAggregator = eventAggregator;
             SelectedFilePath = filePath;
             IpData = new ObservableCollectionPropertyNotify<IPDataModel>();
             IpAddressList = new List<IPAddress>();
             IpDataGrid = new DataGrid();
             uiContext = SynchronizationContext.Current;
+            RefreshRateInputVisibility = Visibility.Hidden;
+            //sets default refresh rate at 500 ms
+            internalRefreshRate = 500;
 
             ParseHTML.ParseIpHTML(IpData, filePath);
             for (int i = 0; i < IpData.Count; i++)
@@ -39,23 +51,85 @@ namespace IPMonitoring.Pages
             }
 
             RefreshIP = true;
-            StartIpPingThread();
-        }
+
+            //~~~~~~~~~~~!!DO NOT CHANGE THIS!!~~~~~~~~~~~~~~~~~~~~~~
+            //There is a bug in .NET frameworks 4 and up that causes a PROCCESS_HAS_LOCKED_PAGES blue screen of death
+            //if you have a debugger attached and stop debugging while the ping is in process
+            //If you want to test ping functionality you must build without debugging (CTRL-F5)
+
+            if (!System.Diagnostics.Debugger.IsAttached)
+            {
+                StartIpPingThread();
+            }
+
+            //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        } 
+        #endregion
+
+        #region Properties
 
         public DataGrid IpDataGrid { get; set; }
         public string SelectedFilePath { get; set; }
         public ObservableCollectionPropertyNotify<IPDataModel> IpData { get; set; }
         public bool RefreshIP { get; set; }
+
         public string InputProjectNumber
         {
             get => inputProjectNumber;
             set => inputProjectNumber = value;
         }
-        public void ReturnToMachine_OnClick()
+
+        public string RefreshRateInput
         {
-            this.eventAggregator.Publish(new ReturnToMachine()); 
+            get => refreshRateInput;
+            set => refreshRateInput = value;
         }
 
+
+        public Visibility RefreshRateInputVisibility
+        {
+            get { return this.refreshRateInputVisibilty; }
+            set { SetAndNotify(ref this.refreshRateInputVisibilty, value); }
+        }
+
+        #endregion
+
+        #region Return To Machine
+
+        public void ReturnToMachine_OnClick()
+        {
+
+            //Wait for ping thread to finish before returning to machineselectview
+            RefreshIP = false;
+            PingThread.Join();
+
+            this.eventAggregator.Publish(new ReturnToMachine());
+        }
+        #endregion
+
+        #region Refresh Rate
+        public void AcceptRefreshRate_OnClick()
+        {
+
+            //Wait for ping thread to finish before returning to machineselectview
+            RefreshRateInputVisibility = Visibility.Hidden;
+            Int32.TryParse(RefreshRateInput, out internalRefreshRate);
+        }
+
+        public void SetRefreshRate_OnClick()
+        {
+            RefreshRateInputVisibility = Visibility.Visible;
+        } 
+
+        public void RefreshRateInput_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        #endregion
+
+        #region Ping Functions
 
         public void StartIpPingThread()
         {
@@ -95,13 +169,16 @@ namespace IPMonitoring.Pages
 
                     }
                 }
+                //Set so that the UI is only refreshed if a change in connection state is detected
                 if (UIRefreshNeeded)
                 {
                     IpData.Refresh(uiContext);
                     UIRefreshNeeded = false;
                 }
-                Thread.Sleep(500);
+                Thread.Sleep(internalRefreshRate);
             }
+
         }
+        #endregion
     }
 }
--- IPMonitoring/Pages/ReturnToMachine.cs
+++ IPMonitoring/Pages/ReturnToMachine.cs
@@ -10,7 +10,6 @@ namespace IPMonitoring.Pages
     {
         public ReturnToMachine()
         {
-
         }
     }
 }
